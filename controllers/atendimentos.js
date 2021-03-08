1277f5951a03acf03345e04222daa4f67e512dc9
Gustavo <gumastro@usp.br>
2021-03-08 12:51:59 -0300

    adds POST route to atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Servidor rodando, tudo ok! Rota atendimentos e realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => res.send('Rota de atendimentos e realizando um POST'))
 }
