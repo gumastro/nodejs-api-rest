b4942b2651e4df6350e9794c956b7466ce638e29
Gustavo <gumastro@usp.br>
2021-03-08 12:37:17 -0300

    Modularizes main file using consign

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express')
+const customExpress = require('./config/customExpress')
 
-const app = express()
+const app = customExpress()
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
-
-app.get('/', (req, res) => res.send('Servidor rodando, tudo ok!'))
