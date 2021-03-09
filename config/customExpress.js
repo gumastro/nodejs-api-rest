227d4c4a21d3936ee2717b7e84cd310ed009d6ee
Gustavo <gumastro@usp.br>
2021-03-09 00:00:35 -0300

    Translate requests from urlencoded and json

--- config/customExpress.js
+++ config/customExpress.js
@@ -4,6 +4,12 @@ const consign = require('consign')
 module.exports = () => {
     const app = express()
 
+    app.use(express.json());
+    app.use(express.urlencoded({
+        extended: true
+    }
+    ))
+
     consign()
         .include('controllers')
         .into(app)
