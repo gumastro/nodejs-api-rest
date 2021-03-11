b7ebd84b3477c3fb8451a93d18b9e8a7cf3566f4
Gustavo <gumastro@usp.br>
2021-03-11 17:34:05 -0300

    Add validations
    Add validations on appointments to verify clients name and appointment date and return errors to client, if any

--- models/appointments.js
+++ models/appointments.js
@@ -6,17 +6,41 @@ class Appointment {
     add(appointment, res) {
         const created = new moment().format('YYYY-MM-DD HH:mm:ss')
         const appointmentDate = moment(appointment.appointmentDate, 'DD-MM-YYYY').format('YYYY-MM-DD HH:mm:ss')
-        const bookedAppointment = {...appointment, created, appointmentDate}
+        
+        const isDateValid = moment(appointmentDate).isSameOrAfter(created)
+        const isClientValid = appointment.client.length >= 3
 
-        const sql = 'INSERT INTO Appointments SET ?'
-
-        connection.query(sql, bookedAppointment, (error, results) => {
-            if(error) {
-                res.status(400).json(error)
-            } else {
-                res.status(201).json(results)
+        const validations = [
+            {
+                name: 'date',
+                valid: isDateValid,
+                message: 'Appointment date must be after or equal to current date'
+            },
+            {
+                name: 'client',
+                valid: isClientValid,
+                message: 'Client name must be longer than 2 characters'
             }
-        })
+        ]
+
+        const errors = validations.filter(field => !field.valid)
+
+        if (errors.length) {
+            res.status(400).json(errors)
+        } else {
+            
+            const bookedAppointment = {...appointment, created, appointmentDate}
+
+            const sql = 'INSERT INTO Appointments SET ?'
+
+            connection.query(sql, bookedAppointment, (error, results) => {
+                if(error) {
+                    res.status(400).json(error)
+                } else {
+                    res.status(201).json(results)
+                }
+            })
+        }
     }
 }
 
