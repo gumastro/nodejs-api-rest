95210faaf464d5cde7770d7808c7193b9e45b073
Gustavo <gumastro@usp.br>
2021-03-12 20:56:40 -0300

    Update how files are read and written
    Change read/write buffer to create read/write stream to not block program execution if read/write takes too long

--- files/uploadFiles.js
+++ files/uploadFiles.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
 
-fs.readFile('./assets/wboris.jpeg', (error, buffer) => {
-    console.log('Image has been buffered')
-
-    fs.writeFile('./assets/wboris2.jpeg', buffer, (error) => {
-        console.log('Image has been writen')
+fs.createReadStream('./assets/wboris.jpeg')
+    .pipe(fs.createWriteStream('./assets/wboris-stream.jpeg'))
+    .on('finish', () => {
+        console.log('Image successfully writen')
     })
-})
